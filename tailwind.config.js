8583dbecb1e6defe0759c7c97fed18b40ad8b359
Rene-Roscher <43115138+Rene-Roscher@users.noreply.github.com>
2023-11-25 09:24:45 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
